e298eb4ce848531e2a9309b301c16407058b61f7
staff-gymnast <staff-gymnast@outlook.com>
2024-08-09 15:41:45 +0000

    Update exercise1 and exercise2

--- exercise1/tree.py
+++ exercise1/tree.py
@@ -20,7 +20,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from linked_queue import LinkedQueue
-import collections
 
 class Tree:
   """Abstract base class representing a tree structure."""
